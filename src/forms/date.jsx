774e349860fc232573dfe322e924cc4dab78aecb
Leonard Martin <leonard.martin@gmail.com>
2018-09-20 10:34:50 +0100

    Update date component html
    To bring in line with https://design-system.service.gov.uk/components/date-input/

--- src/forms/date.jsx
+++ src/forms/date.jsx
@@ -21,25 +21,37 @@ class DateInput extends Input {
 
   render() {
     const value = this.parseValue();
-    return <div className={this.errorClass('form-group')}>
-      <fieldset>
-        <legend>
-          <span className="form-label-bold">{this.props.label}</span>
-          { this.props.hint && <span className="form-hint">{this.props.hint}</span> }
-          { this.props.error && <span className="error-message">{this.props.error}</span> }
+    return <div className={this.errorClass('govuk-form-group')}>
+      <fieldset className="govuk-fieldset" aria-describedby={this.dateFragment('hint')} role="group">
+        <legend className="govuk-fieldset__legend">
+          <span className="govuk-label">{this.props.label}</span>
+          { this.props.hint && <span className="govuk-hint" id={this.dateFragment('hint')}>{this.props.hint}</span> }
+          { this.props.error && <span className="govuk-error-message">{this.props.error}</span> }
         </legend>
-        <div className="form-date">
-          <div className="form-group form-group-day">
-            <label className="form-label" htmlFor={this.dateFragment('day')}>Day</label>
-            <input className={this.errorClass('form-control')} id={this.dateFragment('day')} name={this.dateFragment('day')} type="number" pattern="[0-9]*" min="1" max="31" defaultValue={value.day} />
+        <div className="govuk-date-input">
+          <div className="govuk-date-input__item">
+            <div className="govuk-form-group">
+              <label className="govuk-label govuk-date-input__label" htmlFor={this.dateFragment('day')}>
+                Day
+              </label>
+              <input className="govuk-input govuk-date-input__input govuk-input--width-2" id={this.dateFragment('day')} name={this.dateFragment('day')} type="number" pattern="[0-9]*" defaultValue={value.day} />
+            </div>
           </div>
-          <div className="form-group form-group-month">
-            <label className="form-label" htmlFor={this.dateFragment('month')}>Month</label>
-            <input className={this.errorClass('form-control')} id={this.dateFragment('month')} name={this.dateFragment('month')} type="number" pattern="[0-9]*" min="1" max="12" defaultValue={value.month} />
+          <div className="govuk-date-input__item">
+            <div className="govuk-form-group">
+              <label className="govuk-label govuk-date-input__label" htmlFor={this.dateFragment('month')}>
+                Month
+              </label>
+              <input className="govuk-input govuk-date-input__input govuk-input--width-2" id={this.dateFragment('month')} name={this.dateFragment('month')} type="number" pattern="[0-9]*" defaultValue={value.month} />
+            </div>
           </div>
-          <div className="form-group form-group-year">
-            <label className="form-label" htmlFor={this.dateFragment('year')}>Year</label>
-            <input className={this.errorClass('form-control')} id={this.dateFragment('year')} name={this.dateFragment('year')} type="number" pattern="[0-9]*" min="1900" max="2100" defaultValue={value.year} />
+          <div className="govuk-date-input__item">
+            <div className="govuk-form-group">
+              <label className="govuk-label govuk-date-input__label" htmlFor={this.dateFragment('year')}>
+                Year
+              </label>
+              <input className="govuk-input govuk-date-input__input govuk-input--width-4" id={this.dateFragment('year')} name={this.dateFragment('year')} type="number" pattern="[0-9]*" defaultValue={value.year} />
+            </div>
           </div>
         </div>
       </fieldset>
