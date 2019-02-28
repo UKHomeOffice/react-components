c4be53c92c416f78f20be53ef3ec2c570cbfded5
Joe Fitter <joefitter@hotmail.com>
2019-02-28 14:07:07 +0000

    class -> classNAme

--- src/warning/index.jsx
+++ src/warning/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Warning = ({ children }) => (
-  <div class="govuk-warning-text">
-    <span class="govuk-warning-text__icon" aria-hidden="true">!</span>
-    <strong class="govuk-warning-text__text">
+  <div className="govuk-warning-text">
+    <span className="govuk-warning-text__icon" aria-hidden="true">!</span>
+    <strong className="govuk-warning-text__text">
       { children }
     </strong>
   </div>
