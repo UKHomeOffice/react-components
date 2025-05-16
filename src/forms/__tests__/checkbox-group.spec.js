26b7e8dd0782a1fff556268efff5b505284ffcd2
Jeff Horton <jeff.horton@digital.homeoffice.gov.uk>
2025-05-16 17:19:52 +0100

    [ASL-4614] Changes for declaration component
    Provide an option to override the default `span` that wraps hints

--- src/forms/__tests__/checkbox-group.spec.js
+++ src/forms/__tests__/checkbox-group.spec.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import { render } from 'enzyme'
+import React from 'react';
+import { render } from 'enzyme';
 import { CheckboxGroup } from 'ho-react-components';
 
+const EXAMPLE_OPTIONS = [
+  {
+    label: 'one',
+    value: 1
+  },
+  {
+    label: 'two',
+    value: 2
+  },
+  {
+    label: 'three',
+    value: 3,
+    reveal: <p className="revealed">Hello</p>
+  }
+];
 describe('CheckboxGroup', () => {
 
   it('renders checkboxes', () => {
-    const options = [
-      {
-        label: 'one',
-        value: 1
-      },
-      {
-        label: 'two',
-        value: 2
-      },
-      {
-        label: 'three',
-        value: 3,
-        reveal: <p className="revealed">Hello</p>
-      }
-    ];
     const rendered = render(<CheckboxGroup
       label="test"
       name="date"
-      options={options}
+      options={EXAMPLE_OPTIONS}
       value={[ 1, 2 ]}
     />);
     expect(rendered.find('input[type="checkbox"]').length).toEqual(3);
@@ -33,25 +33,10 @@ describe('CheckboxGroup', () => {
   });
 
   it('should not render reveals if `initialHideReveals` is true', () => {
-    const options = [
-      {
-        label: 'one',
-        value: 1
-      },
-      {
-        label: 'two',
-        value: 2
-      },
-      {
-        label: 'three',
-        value: 3,
-        reveal: <p className="revealed">Hello</p>
-      }
-    ];
     const rendered = render(<CheckboxGroup
       label="test"
       name="date"
-      options={options}
+      options={EXAMPLE_OPTIONS}
       value={1}
       initialHideReveals={true}
     />);
@@ -60,25 +45,10 @@ describe('CheckboxGroup', () => {
   });
 
   it('should render reveals if `initialHideReveals` is false', () => {
-    const options = [
-      {
-        label: 'one',
-        value: 1
-      },
-      {
-        label: 'two',
-        value: 2
-      },
-      {
-        label: 'three',
-        value: 3,
-        reveal: <p className="revealed">Hello</p>
-      }
-    ];
     const rendered = render(<CheckboxGroup
       label="test"
       name="date"
-      options={options}
+      options={EXAMPLE_OPTIONS}
       value={1}
       initialHideReveals={false}
     />);
@@ -144,6 +114,43 @@ describe('CheckboxGroup', () => {
     />);
     expect(rendered.find('.govuk-checkboxes__divider-wide').length).toEqual(1);
     expect(rendered.find('.govuk-checkboxes__divider-wide').text()).toEqual('Or select each that applies:');
+  })
+
+  it('renders a default hint wrapper', () => {
+    const rendered = render(<CheckboxGroup
+        label="test"
+        name="date"
+        options={[
+          {
+            label: 'one',
+            value: 1,
+            ['aria-describedby']: 'date-hint',
+          }
+        ]}
+        value={1}
+        hint='Hint in a span'
+    />);
+
+    expect(rendered.find('span#date-hint.govuk-hint').text()).toEqual('Hint in a span');
+  })
+
+  it('should render a custom hint wrapper', () => {
+    const rendered = render(<CheckboxGroup
+        label="test"
+        name="date"
+        options={[
+          {
+            label: 'one',
+            value: 1,
+            ['aria-describedby']: 'date-hint',
+          }
+        ]}
+        value={1}
+        hint={<p>Hint in a paragraph</p>}
+        hintWrapper={({id, children}) => <div id={id}>{children}</div>}
+    />);
+
+    expect(rendered.find('div#date-hint > p').text()).toEqual('Hint in a paragraph');
   })
 
 });
